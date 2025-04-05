74ed073d6e7959974b4db381f31b7ee9cd2b18eb
Rafa <rafamorenoo2004@gmail.com>
2025-04-05 12:41:22 +0200

    Subida de proyecto

--- src/ejercicio4/Libro.java
+++ src/ejercicio4/Libro.java
@@ -2,4 +2,30 @@ package ejercicio4;
 
 public class Libro {
 
+	private String titulo;
+
+	private enum tipo_libro {
+
+		Novela, Teatro, Poesía, Ensayo;
+	}
+
+	private tipo_libro tipo;
+
+	private String editorial;
+	private int año;
+	private int ISBN;
+
+	public Libro(String titulo, tipo_libro tipo, String editorial, int año, int ISBN) {
+
+		this.titulo = titulo;
+		this.tipo = tipo;
+		this.editorial = editorial;
+		this.año = año;
+		this.ISBN = ISBN;
+	}
+
+	public String getNombreAutor() {
+		return "";
+	}
+
 }
